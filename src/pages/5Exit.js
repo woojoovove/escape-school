03f7c9c0fee21bbdad0aa1bb289009d578e84655
woojin <ujinjeong55@gmail.com>
2025-11-15 18:47:41 +0900

    [add] axe logic

--- src/pages/5Exit.js
+++ src/pages/5Exit.js
@@ -1,13 +1,22 @@
 import Layout from "../components/Layout";
 import { useGame } from "../context/GameContext";
+import doorImg from '../img/house_locked.jpg';
+import React from "react";
 
 function Exit() {
-    const { addItem } = useGame();
+    const { items } = useGame();
 
+    const hasAxe = items.includes("도끼");
+    const checkAxe = () => {
+        if (hasAxe) {
+            alert("도끼로 문을 부숩니다.");
+        } else {
+            alert("도끼가 필요합니다.")
+        }
+    }
     return (
         <Layout nextPath="/6Ending">
-            <h1>출구</h1>
-            <button onClick={() => addItem("열쇠")}>열쇠 줍기 🔑</button>
+            <img src={doorImg} onClick={checkAxe}></img>
         </Layout>
     );
 }
